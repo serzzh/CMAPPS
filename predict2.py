39054e743334eb3a6419315367a3608ee2d02e0e
serzzh <serzzh@gmail.com>
2019-07-27 22:02:54 +0300

    Create predict2.py

---
+++ predict2.py
@@ -0,0 +1,17 @@
+from model import *
+from config import *
+from data_processing import *
+today = datetime.date.today()
+
+config = TsConf()
+
+import psycopg2
+conn = psycopg2.connect(dbname='elevator', user='elevator', 
+                        password='elevator', host='95.213.193.6', port = '9432')
+cursor = conn.cursor()
+
+cursor.execute('SELECT * FROM PUBLIC.ELEVATOR')
+records = cursor.fetchall()
+...
+cursor.close()
+conn.close()
