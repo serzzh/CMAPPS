f3ddf039efe50a1ea54a4fc3b87cf3d6da17fb20
serge <serge@172.18.1.26>
2019-07-28 12:19:30 +0300

    1

--- predict.py
+++ predict.py
@@ -80,7 +80,7 @@ print(z.tail(), dtrain.tail())
 
 
 #try:
-postgres_insert_query = """UPDATE RUL SET RUL2 = %i WHERE FILEID = %i AND ENGINEID = %i AND  TIMECYCLE = %i"""
+postgres_insert_query = """UPDATE RUL SET RUL2 = %f WHERE FILEID = %i AND ENGINEID = %i AND  TIMECYCLE = %i"""
 for i in range(len(z)):
     record_to_insert = (z["pred"][i], z["FILEID"][i], z["ENGINEID"][i], z["TIMECYCLE"][i])
     print(postgres_insert_query % record_to_insert)
